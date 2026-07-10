947457051cb35ca687d064b7b326f4f8c1112f2d
hey :) it’s shrey <110947127+bytesizedshrey@users.noreply.github.com>
2026-07-10 20:17:29 +0530

    Refactor GSAP animations in main.js: commented out old animations and implemented fromTo for smoother transitions.

--- day01/src/main.js
+++ day01/src/main.js
@@ -4,16 +4,27 @@ import './style.css'
 const box = document.querySelector('.box')
 
 
-gsap.from('.box',{
-  delay : 0.3,
-  x : 450,
-  duration : 2
-})
+// gsap.from('.box',{
+//   delay : 0.3,
+//   x : 450,
+//   duration : 2
+// })
 
-gsap.to('.box',{
-  delay : 3,
-  x : 600,
-  y : 800,
-  duration : 3,
+// gsap.to('.box',{
+//   delay : 3,
+//   x : 600,
+//   y : 800,
+//   duration : 3,
+// })
+
+//fromTo
+gsap.fromTo(".box",{
+  x : 0,
+},{
+  delay : 0.9,
+  duration : 1,
+  x : 550,
+  y : 550
 })
+ 
 
