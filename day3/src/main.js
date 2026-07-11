7acb3ce4afe643fb3f330db222bcbe3ef335447b
hey :) it’s shrey <110947127+bytesizedshrey@users.noreply.github.com>
2026-07-11 10:43:55 +0530

    stagger done and dusted.

--- day3/src/main.js
+++ day3/src/main.js
@@ -1,13 +1,27 @@
 import gsap from 'gsap'
 import './style.css'
 
-gsap.to('.box',{
-  x : 500,
-  duration : 1.3,
-  ease : 'power4.out',
-  delay : 1.6,
-  stagger : -0.2,
-  yoyo : true,
-  repeat : -1
+// gsap.to('.box',{
+//   x : 500,
+//   duration : 1.3,
+//   ease : 'power4.out',
+//   delay : 1.6,
+//   stagger : {
+//     each : 0.1,
+//     from: 'random'
+//   },
+//   yoyo : true,
+//   repeat : -1
+// })
+
+gsap.from('h1 span',{
+  yPercent : 100,
+  opacity: 0, 
+  duration : 5,
+  ease: 'expo.out',
+  stagger : {
+    each : 0.08,
+    from : 'random'
+  }
 })
 
