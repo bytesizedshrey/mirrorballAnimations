3f6720915d346c661b4379de9eea910c75257401
hey :) it’s shrey <110947127+bytesizedshrey@users.noreply.github.com>
2026-07-11 06:14:37 +0530

    Refactor GSAP animation in main.js: updated animation parameters for improved performance, added callback functions for start and completion events, and adjusted styling in style.css to prevent overflow.

--- day2/src/main.js
+++ day2/src/main.js
@@ -1,12 +1,35 @@
 import gsap from 'gsap'
 import './style.css'
 
+// gsap.set('.box',{
+//   x : -300,
+// })
+
+// gsap.to('.box',{
+//   x : 1500,
+//   duration : 1.5,
+//   delay : 0.6,
+//   ease: "sine.in",
+//   repeat: -1,
+//   // yoyo: true
+// })
+
+//callback functions
+
 gsap.to('.box',{
-  x : 900,
-  duration : 1.5,
-  delay : 0.6,
-  ease: "sine.in",
-  repeat: 3,
-  yoyo: true
+  x : 500,
+  duration : 1.4,
+  delay : 1,
+  ease : 'power2.inOut',
+
+  onStart : () =>{
+    console.log('animation has started...')
+  },
+
+  onComplete : () => {
+    console.log('animation has been completed...')
+  }
 })
+
+
 
