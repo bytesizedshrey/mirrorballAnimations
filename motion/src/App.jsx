7a5506fddd6e44b0a89fa2d182f7e7beab92a635
hey :) it’s shrey <110947127+bytesizedshrey@users.noreply.github.com>
2026-07-12 22:17:08 +0530

    Refactor App component: introduce box variants for improved animation control, update initial and animate properties for smoother transitions, and remove unused viewport and whileInView settings.

--- motion/src/App.jsx
+++ motion/src/App.jsx
@@ -2,20 +2,28 @@ import { motion, scale } from 'motion/react'
 import React from 'react'
 
 const App = () => {
+  const boxVarient = {
+    hidden : {
+      opacity : 0
+    },
+    visible:{
+      opacity : 1,
+      x : 500
+    }
+  }
   return (
     <div>
-      <div className="page"></div>
     <motion.div 
-    initial={{opacity : 0,scale : 0.7}}
+    variants={boxVarient}
+    initial="hidden"
     transition={{duration:1.2, ease: 'easeInOut'}}
-    // animate={{x : 500, opacity: 0.3}} 
+    animate="visible"
     // whileHover={{scale: 1.5}} 
     // whileTap={{scale:0.4}}
-    viewport={{once:false, amount: 1}}
-    whileInView={{opacity:1,scale:1}}
+    // viewport={{once:false, amount: 1}}
+    // whileInView={{opacity:1,scale:1}}
     className='box'>
     </motion.div>
-    <div className="page"></div>
     </div>
   )
 }
