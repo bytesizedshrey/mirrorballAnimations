77f778476c645d93118c6b43983d0141c173b07d
hey :) it’s shrey <110947127+bytesizedshrey@users.noreply.github.com>
2026-07-12 16:11:34 +0530

    Refactor index.html and main.js: clean up heading structure in index.html, enhance GSAP animation parameters in main.js for improved stagger effects, and add new CSS classes for better styling control.

--- day5/src/main.js
+++ day5/src/main.js
@@ -5,16 +5,18 @@ import { SplitText } from "gsap/SplitText";
 gsap.registerPlugin(SplitText);
 
 const split = new SplitText('.title h1',{
-  type : "chars,words,lines"
+  type : "chars,words,lines",
+  wordsClass : 'titleWord',
+  charsClass: 'titleChars'
 })
 
-gsap.from(split.lines,{
+gsap.from(split.chars,{
   yPercent : 100,
   opacity : 0,
   duration : 1.2,
   ease : 'expo.out',
   stagger: {
-    each : 1,
-    from : 'start'
+    each : 0.06,
+    from : 'random'
   }
 })
