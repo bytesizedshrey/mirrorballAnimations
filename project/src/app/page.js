bed26fcecef648753443b9beb1f8a1d67fb1b42c
hey :) it’s shrey <110947127+bytesizedshrey@users.noreply.github.com>
2026-07-13 22:48:56 +0530

    Refactor Home component: replace hover animations with InfiniteCarousel for project display, update layout for full-screen view, and remove unused TextReveal functionality.

--- project/src/app/page.js
+++ project/src/app/page.js
@@ -1,28 +1,14 @@
 "use client"
+import InfiniteCarousel from "@/components/InfiniteCarousel";
 import TextReveal from "@/components/TextReveal";
+import { projects } from "@/data/projects";
 import { useRef } from "react";
 
 export default function Home() {
-  const triggerRef = useRef(null)
-
-  const handleHoverEnter = () => {
-    triggerRef.current?.play()
-  }
-  const handleHoverLeave = () => {
-    triggerRef.current?.reverse()
-  }
 
   return(
-  <main className="bg-black h-[300vh] w-full">
-    <div 
-    onPointerEnter={handleHoverEnter} 
-    onPointerLeave={handleHoverLeave} 
-    className="h-[12rem] w-[12rem] bg-red-950">
-    </div>
-
-     <TextReveal ref={triggerRef} splitBy="chars" trigger="scroll" className='text-[6rem] text-white'>
-      Hello Monza
-     </TextReveal>
+  <main className="bg-black h-screen w-full">
+    <InfiniteCarousel projects={projects}/>
     </main>
   );
 }
