27d3826cf9da231ee30e9e2ad11cc6ffa145d1c8
hey :) it’s shrey <110947127+bytesizedshrey@users.noreply.github.com>
2026-07-15 00:26:45 +0530

    Add Three.js integration: create a basic scene with a rotating cube, update HTML structure to include a canvas, and add Three.js as a dependency in package files.

--- ThreeJS/day01/src/main.js
+++ ThreeJS/day01/src/main.js
@@ -1,1 +1,40 @@
 import './style.css'
+import * as THREE from 'three';
+
+//Scene
+const scene = new THREE.Scene()
+
+//Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth/window.innerHeight,
+  0.01,
+  100
+)
+
+//camera position
+camera.position.z = 5;
+
+//mesh
+const geometry = new THREE.BoxGeometry(1,1,1)//width , height , depth
+const material = new THREE.MeshBasicMaterial({
+  color: 'red',
+})
+
+//actor
+const cube = new THREE.Mesh(geometry,material)
+cube.rotation.y = 1.1
+cube.rotation.x = 1.1
+scene.add(cube)
+//canvas(parda)
+const canvas = document.querySelector("canvas")
+//renderer : which canvas needs to be projected
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+})
+
+//aspect ratio
+renderer.setSize(window.innerWidth,window.innerHeight)
+
+//on
+renderer.render(scene,camera)
