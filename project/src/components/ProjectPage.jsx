6b7153aaf08aa0476b2ff571f1164748535804e1
hey :) it’s shrey <110947127+bytesizedshrey@users.noreply.github.com>
2026-07-14 16:20:14 +0530

    Enhance ProjectPage component with GSAP animations: implement image reveal effect on load, refactor layout structure, and ensure proper use of refs for improved visual dynamics.

--- project/src/components/ProjectPage.jsx
+++ project/src/components/ProjectPage.jsx
@@ -1,53 +1,73 @@
-import TextReveal from "./TextReveal"
+"use client";
+import gsap, { useGSAP } from "@/libs/gsap";
+import TextReveal from "./TextReveal";
+import { useRef } from "react";
 
-const ProjectPage = ({project}) => {
+const ProjectPage = ({ project }) => {
+  const containerRef = useRef(null);
+  const imageRef = useRef(null);
+  useGSAP(() => {
+    gsap.to(imageRef.current,{
+        clipPath: "inset(0% 0 0 0)",
+        scale : 1,
+        duration : 1.4,
+        ease : 'expo.out',
+        delay : 0.9
+    })
+  }, { scope: containerRef });
   return (
     <>
-    <main>
+      <main ref={containerRef}>
         <section className="h-screen flex w-full pt-[7rem] pb-[4rem] px-[3rem]">
-            <div className="firstSegment h-full w-[10%]">
-                <TextReveal>
-                    <h3 className="text-[2rem]">{project.number}</h3>
-                </TextReveal>
-                </div>
-                <div className="secondSegment h-[85%] w-[30%]">
-                    <div className="imageDiv h-full w-full">
-                        <img 
-                        className="h-full w-full object-cover"
-                        src={project.coverImage} 
-                        alt=""
-                        />
-                </div>
+          <div className="firstSegment h-full w-[10%]">
+            <TextReveal>
+              <h3 className="text-[2rem]">{project.number}</h3>
+            </TextReveal>
+          </div>
+          <div className="secondSegment h-[85%] w-[30%]">
+            <div className="imageDiv overflow-hidden h-full w-full">
+              <img
+              ref={imageRef}
+                style={{
+                  clipPath: "inset(100% 0 0 0)",
+                }}
+                className="h-full scale-[1.4] w-full object-cover"
+                src={project.coverImage}
+                alt=""
+              />
             </div>
-            <div className="thirdSegment pl-[8rem] h-[85%] w-[60%] flex flex-col justify-end">
-                <div className="heading">
-                    <TextReveal delay='0.85' ease='power4.out' splitBy='chars'>
-                        <h1 className="text-[5rem] leading-[1.1]">{project.title}</h1>
-                    </TextReveal>
-                </div>
-                <div className="subHeading flex gap-[3rem]">
-                    <TextReveal delay='0.85' splitBy='words'>
-                        <h1 className="text-[2rem]">{project.subtitle}</h1>
-                    </TextReveal>
-                    <TextReveal delay='0.85' splitBy='chars'>
-                        <h1 className="text-[2rem]">{project.year}</h1>
-                    </TextReveal>
-                </div>
-                <div className="description mt-[2rem] w-[70%] text-balance">
-                    <TextReveal delay='0.85' splitBy='lines'>
-                        <p className="text-[1.5rem] leading-[1.2]">{project.description}</p>
-                    </TextReveal>
-                </div>
+          </div>
+          <div className="thirdSegment pl-[8rem] h-[85%] w-[60%] flex flex-col justify-end">
+            <div className="heading">
+              <TextReveal delay="0.85" ease="power4.out" splitBy="chars">
+                <h1 className="text-[5rem] leading-[1.1]">{project.title}</h1>
+              </TextReveal>
             </div>
+            <div className="subHeading flex gap-[3rem]">
+              <TextReveal delay="0.85" splitBy="words">
+                <h1 className="text-[2rem]">{project.subtitle}</h1>
+              </TextReveal>
+              <TextReveal delay="0.85" splitBy="chars">
+                <h1 className="text-[2rem]">{project.year}</h1>
+              </TextReveal>
+            </div>
+            <div className="description mt-[2rem] w-[70%] text-balance">
+              <TextReveal delay="0.85" splitBy="lines">
+                <p className="text-[1.5rem] leading-[1.2]">
+                  {project.description}
+                </p>
+              </TextReveal>
+            </div>
+          </div>
         </section>
         <section></section>
         <section></section>
         <section></section>
         <section></section>
         <footer></footer>
-    </main>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default ProjectPage
+export default ProjectPage;
