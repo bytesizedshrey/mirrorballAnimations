8aab89db0d93c02a4d7038b16d9f14ef741a4133
hey :) it’s shrey <110947127+bytesizedshrey@users.noreply.github.com>
2026-07-15 17:37:44 +0530

    Enhance Three.js scene with additional loaders and material properties: integrate RGBELoader for environment mapping, update material settings for improved visual quality, and prepare for GLTF model loading with commented-out code for future implementation.

--- ThreeJS/day01/src/main.js
+++ ThreeJS/day01/src/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader, RGBELoader, DRACOLoader } from 'three/examples/jsm/Addons.js';
 
 const size = {
   width: window.innerWidth,
@@ -12,9 +13,7 @@ const scene = new THREE.Scene()
 const clock = new THREE.Clock()
 
 //texture loader
-
 const textureLoader = new THREE.TextureLoader()
-
 const texture = textureLoader.load('https://images.unsplash.com/photo-1531850959096-cfbb6f26c5a8?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   ()=> {
     console.log('texture is loaded')
@@ -26,8 +25,53 @@ const texture = textureLoader.load('https://images.unsplash.com/photo-1531850959
     console.log('error')
   }
 )
-
 const texture2 = textureLoader.load('https://images.unsplash.com/photo-1593544340816-93d84a106415?q=80&w=2390&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
+
+//rgbeLoader
+const rgbeLoader = new RGBELoader()
+rgbeLoader.load('./envMap.hdr', 
+  (texture)=>{
+    texture.mapping = THREE.EquirectangularReflectionMapping
+
+    // scene.background = texture
+    scene.environment = texture
+  },
+  ()=>{
+    console.log('rgbe is loading')
+  },
+  (err)=>{
+    console.error('error loading rgbe environment map', err)
+  }
+)
+
+// //GLTF loader
+// let mixer = null;
+// const gltfLoader = new GLTFLoader();
+
+// // Configure Draco Loader for compressed model
+// const dracoLoader = new DRACOLoader();
+// dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.7/');
+// gltfLoader.setDRACOLoader(dracoLoader);
+
+// gltfLoader.load("./LittlestTokyo.glb",(gltf)=>{
+//   const model = gltf.scene;
+
+//   // Scale down LittlestTokyo as it is originally huge
+//   model.scale.set(0.01, 0.01, 0.01);
+//   model.position.y = -1
+
+//   console.log(gltf.animations)
+
+//   if (gltf.animations && gltf.animations.length > 0) {
+//     mixer = new THREE.AnimationMixer(model)
+//     const action = mixer.clipAction(gltf.animations[0])
+//     action.play()
+//   }
+
+//   scene.add(model)
+// }, undefined, (err) => {
+//   console.error("Error loading glTF model:", err);
+// })
 
 //Camera
 const camera = new THREE.PerspectiveCamera(
@@ -45,24 +89,24 @@ const ambientLight = new THREE.AmbientLight('#ffffff',1.2)
 scene.add(ambientLight)
 
 //directional light
-const directionalLight = new THREE.DirectionalLight("#ffffff",3)
+// const directionalLight = new THREE.DirectionalLight("#ffffff",3)
 
-directionalLight.position.set(1,1,1)
+// directionalLight.position.set(1,1,1)
 
-scene.add(directionalLight)
+// scene.add(directionalLight)
 
-const DirectionalLightHelper = new THREE.DirectionalLightHelper(directionalLight)
-scene.add(DirectionalLightHelper)
+// const DirectionalLightHelper = new THREE.DirectionalLightHelper(directionalLight)
+// scene.add(DirectionalLightHelper)
 
-const pointLight = new THREE.PointLight('#ffffff',2,1.2,1)
+// const pointLight = new THREE.PointLight('#ffffff',2,1.2,1)
 
-pointLight.position.set(0,2,0)
+// pointLight.position.set(0,2,0)
 
-scene.add(pointLight)
+// scene.add(pointLight)
 
-const pointLightHelper = new THREE.PointLightHelper(pointLight)
+// const pointLightHelper = new THREE.PointLightHelper(pointLight)
 
-scene.add(pointLightHelper)
+// scene.add(pointLightHelper)
 
 
 //mesh
@@ -74,7 +118,9 @@ const geometry = new THREE.CapsuleGeometry( 1, 1, 4, 8, 1 );
 // })
 
 const material = new THREE.MeshStandardMaterial({
-  color: 'red'
+  color: 'red',
+  metalness : 0.999,
+  roughness : 0.1
 })
 
 //actor
@@ -91,7 +137,9 @@ const canvas = document.querySelector("canvas")
 //renderer : which canvas needs to be projected
 const renderer = new THREE.WebGLRenderer({
   canvas,
+  antialias: true,
 })
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.enableDamping = true 
@@ -114,9 +162,13 @@ window.addEventListener('resize',()=>{
 
 //ANIMATE
 const animate = () => {
-  const delta = clock.getElapsedTime()
+  const delta = clock.getDelta()
 
-  // cube.rotation.y = delta;
+  // if (mixer) {
+  //   mixer.update(delta)
+  // }
+
+  // cube.rotation.y = clock.getElapsedTime();
 
   controls.update();
 
