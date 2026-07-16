fde450a93ef3ff306b3f360ea21d07c21bbf7ed5
hey :) it’s shrey <110947127+bytesizedshrey@users.noreply.github.com>
2026-07-16 15:42:52 +0530

    Implement ImagePlane component with dynamic texture loading and geometry adjustments: utilize useTexture for texture management, and optimize geometry creation with useMemo to adjust the plane's origin based on provided dimensions.

--- projectR3F/src/components/ImagePlane.jsx
+++ projectR3F/src/components/ImagePlane.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import { useTexture } from '@react-three/drei'
+import React, { useMemo } from 'react'
+import * as THREE from 'three'
+
+const texture = useTexture(url)
 
 const ImagePlane = ({url,position,rotation,planeWidth,planeHeight}) => {
+    //only changes when dependencies does
+    const geometry = useMemo(()=>{
+        //to change default origin 
+        const geo = new THREE.PlaneGeometry(planeWidth, planeHeight)
+        //change transform origin
+        geo.translate(0,planeHeight / 2,0)
+        return geo
+    },[planeHeight,planeWidth])
   return (
-    <>
-    
-    </>
+    <mesh position={position} rotation={rotation} geometry={geometry}>
+        <meshStandardMaterial map={texture} side={THREE.DoubleSide}/>
+    </mesh>
   )
 }
 
